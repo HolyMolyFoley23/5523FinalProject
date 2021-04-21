de500a2e0029f5a66dff92632a824c36b3269514
John Foley <foley406@umn.edu>
2021-04-20 19:34:34 -0500

    Hi

--- final_project_NR.py
+++ final_project_NR.py
@@ -9,7 +9,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 #data_path = './winequalityN.csv'
-data_path = r'C:\Users\noahp\OneDrive\Data Mining\Group Project\Data\winequalityN.csv'
+#HI 
+data_path = r'winequalityN.csv'
 wine = pd.read_csv(data_path)
 
 # %%
